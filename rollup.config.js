40251ec178cb15a74799ed84ce8f16a52b2a0781
Unknown <vlascik@gmail.com>
2018-10-02 08:41:47 +0200

    Package the library with 2012-2022 data as a single file too

--- rollup.config.js
+++ rollup.config.js
@@ -10,6 +10,13 @@ export default [
     }
   },
   {
+    input: 'src/index-2012-2022.js',
+    output: {
+      file: 'dist/index-2012-2022.js',
+      format: 'cjs'
+    }
+  },
+  {
     input: 'src/lookup-convert.js',
     output: {
       file: 'dist/lookup-convert.js',
@@ -34,6 +41,19 @@ export default [
     input: 'src/index.js',
     output: {
       file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'timezone-support',
+      sourcemap: true
+    },
+    plugins: [
+      babel({ exclude: 'node_modules/**' }),
+      uglify()
+    ]
+  },
+  {
+    input: 'src/index-2012-2022.js',
+    output: {
+      file: 'dist/index-2012-2022.umd.js',
       format: 'umd',
       name: 'timezone-support',
       sourcemap: true
